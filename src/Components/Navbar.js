89df58f376eb457f73444298ae9a44e7944498d7
doukaan <114756111+doukaan@users.noreply.github.com>
2024-10-28 18:23:02 +0300

    NEW SİTE!

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,14 +7,14 @@ export default function Navbar() {
 
     return (
         <div className="w-full h-fit pt-5">
-            {/* Desktop & Mobile Navbar */}
+
             <div className="flex flex-row justify-between items-center px-4 md:px-8 lg:px-16">
-                {/* Logo */}
+
                 <div className="flex justify-start md:ml-32">
                     <img src="/İmages/doukaan.png" alt="logo" width={141} height={34} />
                 </div>
 
-                {/* Desktop Menu */}
+
                 <div className="hidden md:flex justify-center gap-8 mr-32">
                     <a href="#About" className="btn btn-outline text-white text-lg hover:text-[#e40f31]">
                         About
@@ -27,7 +27,7 @@ export default function Navbar() {
                     </a>
                 </div>
 
-                {/* Mobile Menu Toggle */}
+
                 <div className="md:hidden">
                     <button
                         onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -38,7 +38,7 @@ export default function Navbar() {
                 </div>
             </div>
 
-            {/* Mobile Menu */}
+
             {isMenuOpen && (
                 <div className="md:hidden absolute w-full bg-[#1A1A1A] z-50 py-4 px-4 mt-4 border-t border-gray-800">
                     <div className="flex flex-col gap-4">
